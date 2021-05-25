1bb6847fb6aa316bc5abe48b6cce58974b9c74c2
Felipe Carrasco <felipe@bakers.dev>
2021-05-24 21:01:26 -0400

    Initial Commit

---
+++ ListUser_SwiftUI/ListUser_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListUser_SwiftUIApp.swift
+//  ListUser_SwiftUI
+//
+//  Created by Pipe Carrasco on 24-05-21.
+//
+
+import SwiftUI
+
+@main
+struct ListUser_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
