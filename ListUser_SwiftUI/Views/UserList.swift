dcefda48ed350073c99cdbeed602b5342a63dd01
Felipe Carrasco <felipe@bakers.dev>
2021-05-25 00:45:51 -0400

    feature: add alert custom

--- ListUser_SwiftUI/Views/UserList.swift
+++ ListUser_SwiftUI/Views/UserList.swift
@@ -9,29 +9,40 @@ import SwiftUI
 
 struct UserList: View {
     @State var usersList: [UserModel] = []
+    @State var showAlert = false
+    @State var btnClicked: ClickedButton = .none
+    @State var isSucess = false
+    @State var message: String = ""
+    
     var body: some View {
-        NavigationView{
-            List(usersList){ user in
-                NavigationLink(
-                    destination: UserDetail(user: user)){
-                    UserRow(user: user)
-                }
-               
-            }.navigationTitle("User list")
-            .onAppear{
-                NetworkClient().getAllUsers { result in
-                    switch result {
-                    case .failure:
-                        //                  ErrorPresenter.showError(message: "There was an error getting the users", on: self)
-                        print("There was an error getting the users")
-                    case .success(let users):
-                        print("users \(users)")
-                        self.usersList = users
+        ZStack{
+            NavigationView{
+                List(usersList){ user in
+                    NavigationLink(
+                        destination: UserDetail(user: user)){
+                        UserRow(user: user)
                     }
+                   
+                }.navigationTitle("User list")
+                .onAppear{
+                    NetworkClient().getAllUsers { result in
+                        switch result {
+                        case .failure:
+                            showAlert = true
+                            message = "There was an error getting the users"
+                        case .success(let users):
+                            self.usersList = users
+                        }
+                    }
+                    
                 }
-                
+            }.blur(radius: showAlert ? 30: 0)
+            
+            if showAlert {
+                AlertMessage(show: $showAlert, btnClicked: $btnClicked, isSuccess: isSucess, message: message)
             }
         }
+        
         
     }
 }
